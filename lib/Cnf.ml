ec03cebbe72c85a76be97b5077c4d385b6951d20
zaxioms <zbattleman@gmail.com>
2023-10-22 15:21:47 -0400

    fixed file structure

--- lib/Cnf.ml
+++ lib/Cnf.ml
@@ -57,109 +57,94 @@ module Clause : CLAUSE = struct
         name ^ pp_clause cs
 end
 
-module type CNF = sig
-  type t
+module Set = Set.Make (String)
 
-  val from_dimacs : string -> t
-  val to_dimacs : t -> (string, int) Hashtbl.t * string
-  val add_clause : Clause.t -> t -> t
-  val add_clauses : Clause.t list -> t -> t
-  val from_clauses : Clause.t list -> t
-  val stats : t -> int * int
-  val empty : unit -> t
-  val pp_cnf : t -> string
-end
+type t = { clauses : Clause.t list; vars : Set.t; num_clauses : int }
+type stats = { num_vars : int; num_clauses : int }
 
-module Cnf : CNF = struct
-  module Set = Set.Make (String)
+let from_dimacs filename =
+  (* read_file copied from stack overflow *)
+  let read_file filename =
+    let lines = ref [] in
+    let chan = open_in filename in
+    try
+      while true do
+        lines := input_line chan :: !lines
+      done;
+      !lines
+    with End_of_file ->
+      close_in chan;
+      List.rev !lines
+  in
+  let spec, lines =
+    match read_file filename with
+    | [] -> failwith "DIMACS file is wrong"
+    | x :: xs -> (x, xs)
+  in
+  let parse_spec spec =
+    let stat = String.split_on_char ' ' (String.trim spec) in
+    match stat with
+    | [ _; _; num_vars; num_clauses ] ->
+        (int_of_string num_vars, int_of_string num_clauses)
+    | _ -> failwith "DIMACS parsing fail"
+  in
+  let _, num_clauses = parse_spec spec in
+  let lines = List.map (fun s -> String.sub s 0 (String.length s - 2)) lines in
+  let clauses = List.map Clause.from_string lines in
+  let hs = Set.empty in
+  let flatten_clauses = List.flatten clauses in
+  let hs = List.fold_right (fun (_, x) s -> Set.add x s) flatten_clauses hs in
+  { clauses; vars = hs; num_clauses }
 
-  type t = { clauses : Clause.t list; vars : Set.t; num_clauses : int }
+let to_dimacs (cnf : t) =
+  let ht_init var_set =
+    let ht = Hashtbl.create (Set.cardinal cnf.vars) in
+    let ctr = ref 0 in
+    Set.fold
+      (fun n () ->
+        ctr := !ctr + 1;
+        if not (Hashtbl.mem ht n) then Hashtbl.add ht n !ctr)
+      var_set ();
+    ht
+  in
+  let ht = ht_init cnf.vars in
+  let rec disj_to_string (disj : Clause.t) =
+    match disj with
+    | [] -> ""
+    | x :: xs -> (
+        match x with
+        | true, s -> string_of_int (Hashtbl.find ht s) ^ " " ^ disj_to_string xs
+        | false, s ->
+            "-" ^ string_of_int (Hashtbl.find ht s) ^ " " ^ disj_to_string xs)
+  in
+  let base =
+    Printf.sprintf "p cnf %d %d\n" (Set.cardinal cnf.vars) cnf.num_clauses
+  in
+  let s =
+    List.fold_left (fun s c -> s ^ disj_to_string c ^ "0\n") base cnf.clauses
+  in
+  (ht, s)
 
-  let from_dimacs filename =
-    (* read_file copied from stack overflow *)
-    let read_file filename =
-      let lines = ref [] in
-      let chan = open_in filename in
-      try
-        while true do
-          lines := input_line chan :: !lines
-        done;
-        !lines
-      with End_of_file ->
-        close_in chan;
-        List.rev !lines
-    in
-    let spec, lines =
-      match read_file filename with
-      | [] -> failwith "DIMACS file is wrong"
-      | x :: xs -> (x, xs)
-    in
-    let parse_spec spec =
-      let stat = String.split_on_char ' ' (String.trim spec) in
-      match stat with
-      | [ _; _; num_vars; num_clauses ] ->
-          (int_of_string num_vars, int_of_string num_clauses)
-      | _ -> failwith "DIMACS parsing fail"
-    in
-    let _, num_clauses = parse_spec spec in
-    let lines =
-      List.map (fun s -> String.sub s 0 (String.length s - 2)) lines
-    in
-    let clauses = List.map Clause.from_string lines in
-    let hs = Set.empty in
-    let flatten_clauses = List.flatten clauses in
-    let hs = List.fold_right (fun (_, x) s -> Set.add x s) flatten_clauses hs in
-    { clauses; vars = hs; num_clauses }
+let add_clause clause cnf =
+  let s =
+    List.fold_left
+      (fun s (_, n) -> if not (Set.mem n s) then Set.add n s else s)
+      cnf.vars clause
+  in
+  {
+    clauses = clause :: cnf.clauses;
+    vars = s;
+    num_clauses = cnf.num_clauses + 1;
+  }
 
-  let to_dimacs (cnf : t) =
-    let ht_init var_set =
-      let ht = Hashtbl.create (Set.cardinal cnf.vars) in
-      let ctr = ref 0 in
-      Set.fold
-        (fun n () ->
-          ctr := !ctr + 1;
-          if not (Hashtbl.mem ht n) then Hashtbl.add ht n !ctr)
-        var_set ();
-      ht
-    in
-    let ht = ht_init cnf.vars in
-    let rec disj_to_string (disj : Clause.t) =
-      match disj with
-      | [] -> ""
-      | x :: xs -> (
-          match x with
-          | true, s ->
-              string_of_int (Hashtbl.find ht s) ^ " " ^ disj_to_string xs
-          | false, s ->
-              "-" ^ string_of_int (Hashtbl.find ht s) ^ " " ^ disj_to_string xs)
-    in
-    let base =
-      Printf.sprintf "p cnf %d %d\n" (Set.cardinal cnf.vars) cnf.num_clauses
-    in
-    let s =
-      List.fold_left (fun s c -> s ^ disj_to_string c ^ "0\n") base cnf.clauses
-    in
-    (ht, s)
+let add_clauses cls_lst cnf =
+  List.fold_right (fun cls cnf_acc -> add_clause cls cnf_acc) cls_lst cnf
 
-  let add_clause clause cnf =
-    let s =
-      List.fold_left
-        (fun s (_, n) -> if not (Set.mem n s) then Set.add n s else s)
-        cnf.vars clause
-    in
-    {
-      clauses = clause :: cnf.clauses;
-      vars = s;
-      num_clauses = cnf.num_clauses + 1;
-    }
+let stats cnf =
+  { num_vars = Set.cardinal cnf.vars; num_clauses = cnf.num_clauses }
 
-  let add_clauses cls_lst cnf =
-    List.fold_right (fun cls cnf_acc -> add_clause cls cnf_acc) cls_lst cnf
+let empty () = { clauses = []; vars = Set.empty; num_clauses = 0 }
+let from_clauses cls_list = add_clauses cls_list (empty ())
 
-  let stats cnf = (Set.cardinal cnf.vars, cnf.num_clauses)
-  let empty () = { clauses = []; vars = Set.empty; num_clauses = 0 }
-  let from_clauses cls_list = add_clauses cls_list (empty ())
-
-  let pp_cnf cnf =
-    List.fold_left (fun s cls -> s ^ Clause.pp_clause cls ^ "\n") "" cnf.clauses
-end
+let pp_cnf cnf =
+  List.fold_left (fun s cls -> s ^ Clause.pp_clause cls ^ "\n") "" cnf.clauses
--- lib/Cnf.mli
+++ lib/Cnf.mli
@@ -1,24 +1,24 @@
-module type CLAUSE =
-  sig
-    type lit = bool * string
-    type t = lit list
-    val add_lit : lit -> t -> t
-    val del_lit : lit -> t -> t
-    val empty : unit -> t
-    val from_string : string -> t
-    val pp_clause : t -> string
-  end
+module type CLAUSE = sig
+  type lit = bool * string
+  type t = lit list
+
+  val add_lit : lit -> t -> t
+  val del_lit : lit -> t -> t
+  val empty : unit -> t
+  val from_string : string -> t
+  val pp_clause : t -> string
+end
+
 module Clause : CLAUSE
-module type CNF =
-  sig
-    type t
-    val from_dimacs : string -> t
-    val to_dimacs : t -> (string, int) Hashtbl.t * string
-    val add_clause : Clause.t -> t -> t
-    val add_clauses : Clause.t list -> t -> t
-    val from_clauses : Clause.t list -> t
-    val stats : t -> int * int
-    val empty : unit -> t
-    val pp_cnf : t -> string
-  end
-module Cnf : CNF
+
+type t
+type stats = { num_vars : int; num_clauses : int }
+
+val from_dimacs : string -> t
+val to_dimacs : t -> (string, int) Hashtbl.t * string
+val add_clause : Clause.t -> t -> t
+val add_clauses : Clause.t list -> t -> t
+val from_clauses : Clause.t list -> t
+val stats : t -> stats
+val empty : unit -> t
+val pp_cnf : t -> string
